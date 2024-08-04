7003387dfd57fa0ac12a3c869160d3d6844e55ce
lupenza <54394832+lupenza@users.noreply.github.com>
2024-08-05 00:12:30 +0300

    footer

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -2,57 +2,26 @@
     <div class="footer__area-three">
         <div class="footer__top-three">
             <div class="container">
+                <div class="row mb-50">
+                    <div class="col-lg-6 footer-div">
+                        <div class="footer-icon-div">
+                            <i class="fa fa-globe text-white" style="font-size: 30px"></i>
+                        </div>
+                        <div>
+                            <p class="footer-div-text">Need Any Support For Tour and Visa</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 footer-div div-divider">
+                        <div class="footer-icon-div">
+                            <i class="fa fa-globe text-white" style="font-size: 30px"></i>
+                        </div>
+                        <div>
+                            <p class="footer-div-text">Need Any Support For Tour and Visa</p>
+                        </div>
+                    </div>
+                </div>
+                <hr>
                 <div class="row">
-                    <div class="col-lg-5 col-md-8">
-                        <div class="footer-widget">
-                            <div class="footer__newsletter-three">
-                                <h2 class="title">Never Miss out on a Moment <br> Apexa us</h2>
-                                <form action="#">
-                                    <input type="email" placeholder="e-mail Type . . .">
-                                    <button class="btn" type="submit">Subscribe</button>
-                                </form>
-                            </div>
-                            <div class="footer__social-three">
-                                <span class="title">Follow Us on:</span>
-                                <ul class="list-wrap">
-                                    <li><a href="javascript:void(0)"><i class="fab fa-facebook-f"></i></a></li>
-                                    <li><a href="javascript:void(0)"><i class="fab fa-twitter"></i></a></li>
-                                    <li><a href="javascript:void(0)"><i class="fab fa-instagram"></i></a></li>
-                                    <li><a href="javascript:void(0)"><i class="fab fa-pinterest-p"></i></a></li>
-                                    <li><a href="javascript:void(0)"><i class="fab fa-youtube"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
-                        <div class="footer-widget">
-                            <h4 class="fw-title">Information</h4>
-                            <div class="footer-link-list">
-                                <ul class="list-wrap">
-                                    <li><a href='about.html'>Information</a></li>
-                                    <li><a href='about.html'>About us</a></li>
-                                    <li><a href='team.html'>Meet our team</a></li>
-                                    <li><a href='contact.html'>Case stories</a></li>
-                                    <li><a href='blog.html'>Latest news</a></li>
-                                    <li><a href='contact.html'>Contact us</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-2 col-lg-4 col-sm-6 col-sm-6">
-                        <div class="footer-widget">
-                            <h4 class="fw-title">Top Links</h4>
-                            <div class="footer-link-list">
-                                <ul class="list-wrap">
-                                    <li><a href='about.html'>How it’s Work</a></li>
-                                    <li><a href='contact.html'>Partners</a></li>
-                                    <li><a href='contact.html'>Testimonials</a></li>
-                                    <li><a href='contact.html'>Case Studies</a></li>
-                                    <li><a href='contact.html'>Pricing</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
                     <div class="col-xl-3 col-lg-4 col-md-6">
                         <div class="footer-widget">
                             <h4 class="fw-title">Location</h4>
@@ -86,6 +55,49 @@
                             </div>
                         </div>
                     </div>
+                    <div class="col-xl-2 col-lg-4 col-sm-6 col-sm-6">
+                        <div class="footer-widget">
+                            <h4 class="fw-title">Top Links</h4>
+                            <div class="footer-link-list">
+                                <ul class="list-wrap">
+                                    <li><a href='about.html'>Home</a></li>
+                                    <li><a href='contact.html'>About Us</a></li>
+                                    <li><a href='contact.html'>Testimonials</a></li>
+                                    <li><a href='contact.html'>Case Studies</a></li>
+                                    <li><a href='contact.html'>Pricing</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5 col-md-8">
+                        <div class="footer-widget">
+                            <div class="footer__newsletter-three">
+                                <h2 class="title">Never Miss out on a Moment <br> Apexa us</h2>
+                                <form action="#">
+                                    <input type="email" placeholder="e-mail Type . . .">
+                                    <button class="btn" type="submit">Subscribe</button>
+                                </form>
+                            </div>
+                            <div class="footer__social-three">
+                                <span class="title">Follow Us on:</span>
+                                <ul class="list-wrap">
+                                    <li><a href="javascript:void(0)"><i class="fab fa-facebook-f"></i></a></li>
+                                    <li><a href="javascript:void(0)"><i class="fab fa-twitter"></i></a></li>
+                                    <li><a href="javascript:void(0)"><i class="fab fa-instagram"></i></a></li>
+                                    <li><a href="javascript:void(0)"><i class="fab fa-pinterest-p"></i></a></li>
+                                    <li><a href="javascript:void(0)"><i class="fab fa-youtube"></i></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-3 col-md-4 col-sm-6">
+                        <div class="footer-widget">
+                            <h4 class="fw-title">Reach Us</h4>
+                            
+                        </div>
+                    </div>
+                    
+                  
                 </div>
             </div>
         </div>
